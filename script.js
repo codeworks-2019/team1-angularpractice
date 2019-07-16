1a26202d857227ba655bd8bfd7185be554e6f67c
kirsten <52458031+kirstenangel@users.noreply.github.com>
2019-07-16 10:47:37 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+var inputElement = document.getElementById('dataToBind');
+var showElement = document.getElementById('showBoundData');
 
+// Add an event listener to listen for keyup event
+inputElement.addEventListener('keyup', function() {
+  showElement.innerHTML = inputElement.value.toString();
+});
